79f40797efa0130464e1d69ba2f106c0f83f83c0
Chimeziri Anyanwu <103989041+Mezirix@users.noreply.github.com>
2023-07-12 01:37:25 +0100

    Update 100-safe_first_element.py

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -7,7 +7,7 @@ from typing import Any, sequence, Union
 def safe_first_element (Ist: sequence [Any]) -> Union[Any, None]:
     '''Retrieves the first element of a sequence if it exists.
     '''
-if Ist:
-    return lst[0]
-else:
-    return None
+    if lst:
+        return lst[0]
+    else:
+        return None
